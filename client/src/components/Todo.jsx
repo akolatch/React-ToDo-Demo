795fb43e61a6f4afdb8117d17978bf9f87bdb8be
akolatch <aaron.kolatch@gmail.com>
2021-05-24 10:50:31 -0400

    fix html

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Todo = ({ todo, deleteTodo, markDone }) => (
-  <li style={{ textDecoration: todo.done && 'line-through' }}>
-    <span onClick={() => markDone(todo)}>{todo.value}</span>
+  <li>
+    <span
+      style={{ textDecoration: todo.done && 'line-through' }}
+      onClick={() => markDone(todo)}
+    >
+      {todo.value}
+    </span>
     <button onClick={() => deleteTodo(todo._id)}>X</button>
   </li>
 );
